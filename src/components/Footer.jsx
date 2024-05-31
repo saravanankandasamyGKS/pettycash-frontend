fa07b2fd603bce113cd0b968e37f608a1abd4e76
saravanankandasamyGKS <gksaravanan008@gmail.com>
2024-05-31 15:21:22 +0530

    UPdate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,16 +11,10 @@ const Footer = () => {
       <div className="container">
         <div className="row align-items-center">
           <div className="col-12 col-md-6 order-md-1">
-            <p className="mb-0">&copy; 2023 SHANMUGAM R. All rights reserved.</p>
+            <p className="mb-0">&copy; 2024 SARAVANAN. All rights reserved.</p>
           </div>
           <div className="col-12 col-md-6 text-md-end order-md-2 mt-3 mt-md-0">
             <div className="d-flex justify-content-center justify-content-md-end">
-              <a href="https://www.linkedin.com/in/shanmugamrskfamily" className="text-light me-3">
-                <i className="fab fa-linkedin fa-2x"></i>
-              </a>
-              <a href="https://github.com/Shanmugamrskfamily" className="text-light">
-                <i className="fab fa-github fa-2x"></i>
-              </a>
             </div>
           </div>
         </div>
