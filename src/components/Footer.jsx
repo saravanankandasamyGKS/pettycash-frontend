bdd37006c1ac94118aacd948ed22f01731029a80
saravanankandasamyGKS <gksaravanan008@gmail.com>
2024-05-31 15:28:09 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 //Footer.jsx
-import React from 'react';
-import 'react-toastify/dist/ReactToastify.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './Header.css';
+import React from "react";
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./Header.css";
 
 const Footer = () => {
   return (
@@ -14,8 +14,7 @@ const Footer = () => {
             <p className="mb-0">&copy; 2024 SARAVANAN. All rights reserved.</p>
           </div>
           <div className="col-12 col-md-6 text-md-end order-md-2 mt-3 mt-md-0">
-            <div className="d-flex justify-content-center justify-content-md-end">
-            </div>
+            <div className="d-flex justify-content-center justify-content-md-end"></div>
           </div>
         </div>
       </div>
